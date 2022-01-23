d64c6c89221444227bfff082bb88c89bbbae08c3
Indra Adi Kusuma <idn_blackhat@outlook.co.id>
2022-01-24 01:10:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">QR code component - Tailwind</h1>
+
+![screenshot](https://i.ibb.co/bXWcXrg/qr-code.png)
+
+## Built With
+
+- HTML
+- Tailwind CSS
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
